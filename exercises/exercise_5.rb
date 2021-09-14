12ed324b4d47cc6d4b2d15d032ebf2e231390c9d
Akhila499 <akhilathota499@gmail.com>
2021-09-14 17:33:58 -0400

    implement all the exercise as per instructions

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+totalrevenue = Store.sum("annual_revenue");
+
+puts totalrevenue;
+
+avg_annual_revenue = totalrevenue / Store.all.count;
+
+puts avg_annual_revenue;
+
+store_morethan_1m = Store.where("annual_revenue > 1000000").count
+
+puts store_morethan_1m
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :first_name, presence: true
+  validates :hourly_rate, numericality:{greater_than: 200} 
+  validates :hourly_rate, numericality:{less_than: 40} 
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+end
+def new_store_via_input 
+  input_store_name = gets.chomp
+  new_store = Store.create(name:input_store_name)
+  new_store.valid?
+  new_store.errors.messages.each do |error|
+    puts error
+  end
+end
+
+new_store_via_input
+
